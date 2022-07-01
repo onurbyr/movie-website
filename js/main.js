dba125725025082978177809e5c0ad712d303d97
onurbyr <byronurr@gmail.com>
2022-07-01 23:18:14 +0300

    Slider

--- js/main.js
+++ js/main.js
@@ -22,3 +22,41 @@ navLinks.forEach((selectedElement) => {
     });
   });
 });
+
+let slideIndex = 1;
+
+const plusSlides = (n) => {
+  showSlides((slideIndex += n));
+};
+
+const currentSlide = (n) => {
+  showSlides((slideIndex = n));
+};
+
+const showSlides = (n) => {
+  console.log(n);
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+};
+
+showSlides(slideIndex);
+
+//Run every each 2 seconds
+setInterval(() => {
+  plusSlides(1);
+}, 2000);
