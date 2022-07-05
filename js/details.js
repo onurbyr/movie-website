a90fa543f90dfeea391181b2de18d5a5e2e79708
onurbyr <byronurr@gmail.com>
2022-07-05 20:03:12 +0300

    Details

---
+++ js/details.js
@@ -0,0 +1,7 @@
+//---Search Container
+const detailsSearchContainer = document.querySelector(".search-container-details")
+const detailsSearchButton = document.querySelector(".search-container-details i")
+detailsSearchButton.addEventListener("click", () => {
+  detailsSearchContainer.classList.toggle('search-container-details-active')
+});
+//Search Container---
