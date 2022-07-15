6d96b3bfcfc8d9e1c565d628df8cc143821226ec
onurbyr <byronurr@gmail.com>
2022-07-15 21:22:39 +0300

    People Details 5

--- js/people-details.js
+++ js/people-details.js
@@ -9,3 +9,20 @@ detailsSearchButton.addEventListener("click", () => {
   detailsSearchContainer.classList.toggle("search-container-details-active");
 });
 //Search Container---
+
+//---Popup
+const popup = document.getElementById("myPopup");
+window.togglePopup = () => {
+  popup.classList.toggle("show");
+};
+
+// detect page click except circle
+const ignoreClickOnMeElement = document.querySelector(".people-details-circle");
+document.addEventListener("click", (event) => {
+  let isClickInsideElement = ignoreClickOnMeElement.contains(event.target);
+  if (!isClickInsideElement) {
+    //Do something click is outside specified element
+    popup.classList.remove("show");
+  }
+});
+//Popup---
